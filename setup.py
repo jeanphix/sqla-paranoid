9a31e2d9204bef1bf7a8c98787cf5db1da90f177
Jean-Philippe Serafin <serafinjp@gmail.com>
2019-02-05 07:56:01 +0100

    Bump 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 with open('README.rst', 'r') as f:
     setup(
         name='sqla-paranoid',
-        version='0.1.0',
+        version='0.1.1',
         description='Brings transparent soft delete to SQLAlchemy ORM',
         long_description=f.read(),
         author='jean-philippe serafin',
